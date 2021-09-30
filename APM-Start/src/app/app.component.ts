9473d0c9655a6dd23a3fabef30defe13765a1712
tenzin.dhedon <tendhedon@gmail.com>
2021-09-30 15:07:41 -0300

    Added spinner to every route load or reacted to routing events

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, Event, NavigationStart, NavigationEnd, NavigationError, NavigationCancel } from '@angular/router';
 
 import { AuthService } from './user/auth.service';
 import { slideInAnimation } from './app.animation'
@@ -12,6 +12,7 @@ import { slideInAnimation } from './app.animation'
 })
 export class AppComponent {
   pageTitle = 'J.D. Irving';
+  loading = true;
 
   get isLoggedIn(): boolean {
     return this.authService.isLoggedIn;
@@ -27,7 +28,23 @@ export class AppComponent {
   constructor(
     private authService: AuthService,
     private router: Router
-    ) { }
+    ) {
+    router.events.subscribe((routerEvent: Event) =>{
+      this.checkRouterEvent(routerEvent);
+    });
+  }
+
+  checkRouterEvent(routerEvent: Event): void {
+    if (routerEvent instanceof NavigationStart) {
+      this.loading = true;
+    }
+
+    if (routerEvent instanceof NavigationEnd ||
+        routerEvent instanceof NavigationCancel ||
+        routerEvent instanceof NavigationError) {
+          this.loading = false;
+    }
+  }
 
   logOut(): void {
     this.authService.logout();
