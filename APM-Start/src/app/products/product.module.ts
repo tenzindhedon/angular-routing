a5f25efaa1c7c21402b094ef78955ee8f8224d9e
tenzin.dhedon <tendhedon@gmail.com>
2021-09-30 13:27:43 -0300

    Implemented componentless route grouping for product components

--- APM-Start/src/app/products/product.module.ts
+++ APM-Start/src/app/products/product.module.ts
@@ -15,27 +15,33 @@ import { SharedModule } from '../shared/shared.module';
     SharedModule,
     RouterModule.forChild([
       {
-        path: 'products', component: ProductListComponent
-      },
-      {
-        path: 'products/:id', component: ProductDetailComponent,
-        resolve: { resolvedData: ProductResolver }
-      },
-      {
-        path: 'products/:id/edit', component: ProductEditComponent,
-        resolve: { resolvedData: ProductResolver},
+        path: 'products',
         children: [
           {
-            path: '', redirectTo: 'info', pathMatch: 'full'
+            path: '',
+            component: ProductListComponent
           },
           {
-            path: 'info', component: ProductEditInfoComponent
+            path: ':id', component: ProductDetailComponent,
+            resolve: { resolvedData: ProductResolver }
           },
           {
-            path: 'tags', component: ProductEditTagsComponent
-          },
+            path: ':id/edit', component: ProductEditComponent,
+            resolve: { resolvedData: ProductResolver},
+            children: [
+              {
+                path: '', redirectTo: 'info', pathMatch: 'full'
+              },
+              {
+                path: 'info', component: ProductEditInfoComponent
+              },
+              {
+                path: 'tags', component: ProductEditTagsComponent
+              },
+            ]
+          }
         ]
-      },
+      }
     ])
   ],
   declarations: [
