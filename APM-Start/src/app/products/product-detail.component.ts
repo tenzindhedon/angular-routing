bd796e3a3ae76d07dc3e05e3c196080f98239480
tenzin.dhedon <tendhedon@gmail.com>
2021-09-30 10:05:35 -0300

    Added a resolver to handle errors for product details page

--- APM-Start/src/app/products/product-detail.component.ts
+++ APM-Start/src/app/products/product-detail.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
-import { Product } from './product';
-import { ProductService } from './product.service';
+import { Product, ProductResolved } from './product';
+// import { ProductService } from './product.service';
 
 @Component({
   templateUrl: './product-detail.component.html',
@@ -14,21 +14,24 @@ export class ProductDetailComponent implements OnInit {
   errorMessage: string;
 
   constructor(
-    private productService: ProductService,
+    // private productService: ProductService,
     private route: ActivatedRoute
     ) { }
 
     ngOnInit(): void {
-      const id = +this.route.snapshot.paramMap.get('id');
-      this.getProduct(id);
+      // const id = +this.route.snapshot.paramMap.get('id');
+      // this.getProduct(id);
+      const resolvedData: ProductResolved = this.route.snapshot.data['resolvedData'];
+      this.errorMessage = resolvedData.error;
+      this.onProductRetrieved(resolvedData.product);
     }
 
-  getProduct(id: number): void {
-    this.productService.getProduct(id).subscribe({
-      next: product => this.onProductRetrieved(product),
-      error: err => this.errorMessage = err
-    });
-  }
+  // getProduct(id: number): void {
+  //   this.productService.getProduct(id).subscribe({
+  //     next: product => this.onProductRetrieved(product),
+  //     error: err => this.errorMessage = err
+  //   });
+  // }
 
   onProductRetrieved(product: Product): void {
     this.product = product;
